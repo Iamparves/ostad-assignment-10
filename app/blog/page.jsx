b5870fd93f907924823a9a692d6707d64e7cc639
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2023-08-23 17:40:27 +0600

    blog page done

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,8 +1,26 @@
-const Page = () => {
+import BlogCard from "@/components/BlogCard";
+import { getBlogs } from "@/utils/apiRquest";
+
+const Page = async () => {
+  const blogs = await getBlogs();
+
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
+    <section>
+      <div className="bg-[url('/blog-top.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="bg-black/60 py-16 sm:py-20">
+          <h2 className="text-center font-source_serif text-3xl font-bold text-white sm:text-4xl">
+            Blog
+          </h2>
+        </div>
+      </div>
+      <div className="container py-12 md:py-14 lg:py-20">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+          {blogs.map((blog) => (
+            <BlogCard blog={blog} key={blog.id} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
